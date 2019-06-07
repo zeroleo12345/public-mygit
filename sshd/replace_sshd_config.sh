97e5a6a523711a16be16c9b37edc5e91835fe8d7
zeroleo12345 <zeroleo12345@163.com>
2019-06-07 11:32:09 +0800

    [ADDED] 增加替换sshd配置文件的脚本

---
+++ sshd/replace_sshd_config.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# 替换sshd服务配置文件: sshd_config 内的相关参数, 以令其可以用root登录!
+
+cp  /etc/ssh/sshd_config  /etc/ssh/sshd_config.bak
+
+sed -i'' 's/^#PasswordAuthentication [a-z]\{1,9\}$/PasswordAuthentication yes/g' sshd_config
+sed -i'' 's/^#PermitRootLogin [a-z]\{1,9\}$/PermitRootLogin yes/g' sshd_config
+sed -i'' 's/^#GSSAPIAuthentication [a-z]\{1,9\}$/GSSAPIAuthentication no/g' sshd_config
+sed -i'' 's/^#UsePAM [a-z]\{1,9\}$/UsePAM no/g' sshd_config
+ 
+sed -i'' 's/^#ClientAliveInterval [0-9]\{1,9\}$/ClientAliveInterval 60/g' sshd_config
+sed -i'' 's/^#ClientAliveCountMax [0-9]\{1,9\}$/ClientAliveCountMax 3/g' sshd_config
+sed -i'' 's/^#MaxSessions [0-9]\{1,9\}$/MaxSessions 30/g' sshd_config
+
+diff  /etc/ssh/sshd_config  /etc/ssh/sshd_config.bak
