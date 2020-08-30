718dfd272e5d13d4adf255b46a068c0e1d8b34e3
zeroleo12345 <zeroleo12345@163.com>
2020-08-30 12:05:57 +0800

    m

--- sshd/replace_sshd_config.sh
+++ sshd/replace_sshd_config.sh
@@ -3,14 +3,14 @@
 
 cp  /etc/ssh/sshd_config  /etc/ssh/sshd_config.bak
 
-sed -i'' 's/^#PasswordAuthentication [a-z]\{1,9\}$/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sed -i'' 's/^#PermitRootLogin [a-z]\{1,9\}$/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i'' 's/^#*PasswordAuthentication [a-z]\{1,9\}$/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i'' 's/^#*PermitRootLogin [a-z]\{1,9\}$/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i'' 's/^#GSSAPIAuthentication [a-z]\{1,9\}$/GSSAPIAuthentication no/g' /etc/ssh/sshd_config
-sed -i'' 's/^#UsePAM [a-z]\{1,9\}$/UsePAM no/g' /etc/ssh/sshd_config
+sed -i'' 's/^#*UsePAM [a-z]\{1,9\}$/UsePAM no/g' /etc/ssh/sshd_config
  
-sed -i'' 's/^#ClientAliveInterval [0-9]\{1,9\}$/ClientAliveInterval 60/g' /etc/ssh/sshd_config
-sed -i'' 's/^#ClientAliveCountMax [0-9]\{1,9\}$/ClientAliveCountMax 3/g' /etc/ssh/sshd_config
-sed -i'' 's/^#MaxSessions [0-9]\{1,9\}$/MaxSessions 30/g' /etc/ssh/sshd_config
+sed -i'' 's/^#*ClientAliveInterval [0-9]\{1,9\}$/ClientAliveInterval 60/g' /etc/ssh/sshd_config
+sed -i'' 's/^#*ClientAliveCountMax [0-9]\{1,9\}$/ClientAliveCountMax 3/g' /etc/ssh/sshd_config
+sed -i'' 's/^#*MaxSessions [0-9]\{1,9\}$/MaxSessions 30/g' /etc/ssh/sshd_config
 
 set -o verbose
 diff  /etc/ssh/sshd_config  /etc/ssh/sshd_config.bak
